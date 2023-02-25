2619a497a1d84146dc8df1c7100246151560b147
yujiniii <50348197+yujiniii@users.noreply.github.com>
2023-02-26 00:22:48 +0900

    chore: PORT 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeORMConfig } from './config/typeORM.config';
 import { PostModule } from './post/post.module';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(TypeORMConfig), PostModule],
+  imports: [UserModule, TypeOrmModule.forRoot(TypeORMConfig), PostModule, StoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
