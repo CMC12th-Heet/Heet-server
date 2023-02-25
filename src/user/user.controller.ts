5494839dbc0743422114c8091b7c6ddc1960e2eb
yujiniii <50348197+yujiniii@users.noreply.github.com>
2023-02-26 00:21:58 +0900

    swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -26,13 +26,6 @@ export class UserController {
     return this.userService.emailVerify(verifyUserDto);
   }
 
-  @Post()
-  @ApiOperation({ summary: '회원가입 API', description: '유저를 생성한다.' })
-  @ApiBody({ description: '이것저것,,', type: CreateUserDto })
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
   @Post('/find-duplicate')
   @ApiOperation({ summary: '아이디 중복확인 API', description: '중복확인' })
   @ApiQuery({ description: '입력한 아이디', name: 'username' })
@@ -40,23 +33,30 @@ export class UserController {
     return this.userService.findDuplicateUsername(username);
   }
 
+  @Post()
+  @ApiOperation({ summary: '회원가입 API', description: '유저를 생성한다.' })
+  @ApiBody({ description: '회원가입용 정보', type: CreateUserDto })
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
   @Post('/login')
-  @ApiOperation({ summary: '로그인 API', description: '로그인한다.' })
+  @ApiOperation({
+    summary: '(TOKEN GET) 로그인 API',
+    description: '로그인한다.',
+  })
   @ApiBody({ description: '이메일 주소, 비밀번호', type: LoginUserDto })
   verify(@Body() loginUserDto: LoginUserDto) {
     return this.userService.login(loginUserDto);
   }
 
   @Get('/city')
-  @ApiOperation({ summary: '주소지 선택 API', description: '선택한다' })
-  @ApiQuery({ description: '지역구', name: 'name' })
+  @ApiOperation({
+    summary: '내 동네 선택 API',
+    description: '지역에 따른 00구 정보를 가져온다',
+  })
+  @ApiQuery({ description: '지역구', name: 'name', enum: CityEnum })
   showCity(@Query('name') name: string) {
     return this.userService.showCity(name);
-  }
-
-  @UseGuards(AuthGuard())
-  @Get()
-  findUserDetail(@GetUser() getUser) {
-    return this.userService.tokenCheck(getUser);
   }
 }
