e279d9fbf55c19b9d8bc62099de7e48111a01752
yujiniii <50348197+yujiniii@users.noreply.github.com>
2023-02-26 00:22:32 +0900

    swgger

--- src/user/dto/login-user.dto.ts
+++ src/user/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
+  @ApiProperty({ description: '이메일', default: 'aa@aa.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: '비밀번호', default: '1234' })
   @IsNotEmpty()
   password: string;
 }
