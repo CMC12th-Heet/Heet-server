dc8f0f516580f6ee20a3a6cf58ac50b8fa9251b4
yujiniii <50348197+yujiniii@users.noreply.github.com>
2023-03-03 18:28:40 +0900

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeORMConfig } from './config/typeORM.config';
 import { PostModule } from './post/post.module';
 import { StoreModule } from './store/store.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(TypeORMConfig), PostModule, StoreModule],
+  imports: [UserModule, TypeOrmModule.forRoot(TypeORMConfig), PostModule, StoreModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/typeORM.config.ts
+++ src/config/typeORM.config.ts
@@ -10,6 +10,7 @@ export const TypeORMConfig: TypeOrmModuleOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  timezone: 'Z',
   logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'], //[__dirname + '/../**/*.entities{.ts,.js}'],
   synchronize: true,
